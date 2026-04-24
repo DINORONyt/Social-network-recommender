9ddb378fd070f4b546154fd69b4a9cca660214bc
Егор Ленин <edlenin07@gmail.com>
2026-04-25 01:08:19 +0300

    написал класс User

--- ClassLibrary1/User.cs
+++ ClassLibrary1/User.cs
@@ -1,7 +1,17 @@
-﻿namespace ClassLibrary1
+﻿using System.Collections.Generic;
+
+namespace SocialNetwork.Core
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public HashSet<int> Friends { get; set; } = new();
 
+        public User(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }
