b0b6a278dd747447a8137490504d90caf812d53a
Егор Ленин <edlenin07@gmail.com>
2026-04-25 03:15:59 +0300

    изменил интерфейс программы

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -35,17 +35,12 @@
             this.colCoeff = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colFriends = new System.Windows.Forms.DataGridViewTextBoxColumn();
 
-            this.groupBoxVisual = new System.Windows.Forms.GroupBox();
-            this.pictureBoxNetwork = new System.Windows.Forms.PictureBox();
-
             this.groupBoxStats = new System.Windows.Forms.GroupBox();
             this.lblStats = new System.Windows.Forms.Label();
 
             this.groupBoxControls.SuspendLayout();
             this.groupBoxRecs.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRecs)).BeginInit();
-            this.groupBoxVisual.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNetwork)).BeginInit();
             this.groupBoxStats.SuspendLayout();
             this.SuspendLayout();
 
@@ -58,7 +53,7 @@
             this.labelTitle.Size = new System.Drawing.Size(520, 37);
             this.labelTitle.Text = "Рекомендательная система друзей";
 
-            // groupBoxControls - ИСПРАВЛЕНО: убраны FlatAppearance
+            // groupBoxControls
             this.groupBoxControls.BackColor = panelColor;
             this.groupBoxControls.Controls.Add(this.btnStats);
             this.groupBoxControls.Controls.Add(this.labelUser);
@@ -68,12 +63,11 @@
             this.groupBoxControls.ForeColor = primaryColor;
             this.groupBoxControls.Location = new System.Drawing.Point(12, 55);
             this.groupBoxControls.Name = "groupBoxControls";
-            this.groupBoxControls.Size = new System.Drawing.Size(380, 120);
+            this.groupBoxControls.Size = new System.Drawing.Size(760, 100);
             this.groupBoxControls.TabIndex = 0;
             this.groupBoxControls.TabStop = false;
             this.groupBoxControls.Text = "Управление";
 
-            // labelUser
             this.labelUser.AutoSize = true;
             this.labelUser.Font = new System.Drawing.Font("Segoe UI", 9.75F);
             this.labelUser.ForeColor = Color.FromArgb(64, 64, 64);
@@ -82,61 +76,59 @@
             this.labelUser.Size = new System.Drawing.Size(195, 17);
             this.labelUser.Text = "Выберите пользователя из списка:";
 
-            // comboBoxUsers
             this.comboBoxUsers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxUsers.Font = new System.Drawing.Font("Segoe UI", 9.75F);
             this.comboBoxUsers.FormattingEnabled = true;
             this.comboBoxUsers.Location = new System.Drawing.Point(21, 55);
             this.comboBoxUsers.Name = "comboBoxUsers";
-            this.comboBoxUsers.Size = new System.Drawing.Size(220, 25);
+            this.comboBoxUsers.Size = new System.Drawing.Size(300, 25);
             this.comboBoxUsers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.comboBoxUsers.BackColor = Color.White;
 
-            // btnGenerateRecs
             this.btnGenerateRecs.BackColor = primaryColor;
             this.btnGenerateRecs.FlatAppearance.BorderSize = 0;
             this.btnGenerateRecs.FlatAppearance.MouseOverBackColor = secondaryColor;
             this.btnGenerateRecs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnGenerateRecs.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
             this.btnGenerateRecs.ForeColor = Color.White;
-            this.btnGenerateRecs.Location = new System.Drawing.Point(250, 52);
+            this.btnGenerateRecs.Location = new System.Drawing.Point(420, 52);
             this.btnGenerateRecs.Name = "btnGenerateRecs";
-            this.btnGenerateRecs.Size = new System.Drawing.Size(120, 30);
+            this.btnGenerateRecs.Size = new System.Drawing.Size(150, 30);
             this.btnGenerateRecs.TabIndex = 1;
-            this.btnGenerateRecs.Text = "Найти друзей";
+            this.btnGenerateRecs.Text = "Найти рекомендации";
             this.btnGenerateRecs.UseVisualStyleBackColor = false;
-            this.btnGenerateRecs.Click += new System.EventHandler(this.btnGenerateRecs_Click);
+            this.btnGenerateRecs.Click += new System.EventHandler(this.BtnGenerateRecs_Click);
 
-            // btnStats
             this.btnStats.BackColor = accentColor;
             this.btnStats.FlatAppearance.BorderSize = 0;
             this.btnStats.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, 220, 170);
             this.btnStats.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnStats.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
             this.btnStats.ForeColor = Color.White;
-            this.btnStats.Location = new System.Drawing.Point(250, 85);
+            this.btnStats.Location = new System.Drawing.Point(590, 52);
             this.btnStats.Name = "btnStats";
-            this.btnStats.Size = new System.Drawing.Size(120, 30);
+            this.btnStats.Size = new System.Drawing.Size(150, 30);
             this.btnStats.TabIndex = 2;
             this.btnStats.Text = "Статистика сети";
             this.btnStats.UseVisualStyleBackColor = false;
-            this.btnStats.Click += new System.EventHandler(this.btnStats_Click);
+            this.btnStats.Click += new System.EventHandler(this.BtnStats_Click);
 
-            // groupBoxRecs - ИСПРАВЛЕНО: убраны FlatAppearance
+            // groupBoxRecs
             this.groupBoxRecs.BackColor = panelColor;
             this.groupBoxRecs.Controls.Add(this.dataGridViewRecs);
             this.groupBoxRecs.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
             this.groupBoxRecs.ForeColor = primaryColor;
-            this.groupBoxRecs.Location = new System.Drawing.Point(12, 181);
+            this.groupBoxRecs.Location = new System.Drawing.Point(12, 161);
             this.groupBoxRecs.Name = "groupBoxRecs";
-            this.groupBoxRecs.Size = new System.Drawing.Size(380, 420);
+            this.groupBoxRecs.Size = new System.Drawing.Size(760, 350);
             this.groupBoxRecs.TabIndex = 1;
             this.groupBoxRecs.TabStop = false;
             this.groupBoxRecs.Text = "Топ-10 рекомендаций";
 
-            // dataGridViewRecs
+            // dataGridViewRecs - ИСПРАВЛЕНО: добавлено AutoGenerateColumns = false
             this.dataGridViewRecs.AllowUserToAddRows = false;
             this.dataGridViewRecs.AllowUserToDeleteRows = false;
+            this.dataGridViewRecs.AutoGenerateColumns = false;  // ✅ ГЛАВНОЕ ИСПРАВЛЕНИЕ
             this.dataGridViewRecs.BackgroundColor = Color.White;
             this.dataGridViewRecs.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dataGridViewRecs.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
@@ -156,108 +148,100 @@
             this.dataGridViewRecs.Name = "dataGridViewRecs";
             this.dataGridViewRecs.ReadOnly = true;
             this.dataGridViewRecs.RowHeadersVisible = false;
-            this.dataGridViewRecs.RowTemplate.Height = 30;
+            this.dataGridViewRecs.RowTemplate.Height = 40;
             this.dataGridViewRecs.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridViewRecs.Size = new System.Drawing.Size(374, 396);
+            this.dataGridViewRecs.Size = new System.Drawing.Size(754, 326);
             this.dataGridViewRecs.TabIndex = 0;
+            this.dataGridViewRecs.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
 
             this.dataGridViewRecs.ColumnHeadersDefaultCellStyle.BackColor = primaryColor;
             this.dataGridViewRecs.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
-            this.dataGridViewRecs.ColumnHeadersDefaultCellStyle.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
-            this.dataGridViewRecs.ColumnHeadersHeight = 35;
+            this.dataGridViewRecs.ColumnHeadersDefaultCellStyle.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.dataGridViewRecs.ColumnHeadersDefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridViewRecs.ColumnHeadersHeight = 45;
             this.dataGridViewRecs.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(250, 252, 255);
-            this.dataGridViewRecs.DefaultCellStyle.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.dataGridViewRecs.DefaultCellStyle.Font = new System.Drawing.Font("Segoe UI", 9.5F);
             this.dataGridViewRecs.DefaultCellStyle.ForeColor = Color.FromArgb(64, 64, 64);
+            this.dataGridViewRecs.DefaultCellStyle.Padding = new Padding(8, 4, 8, 4);
+            this.dataGridViewRecs.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
 
-            // colRank
+            // Колонка "№"
             this.colRank.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.colRank.DataPropertyName = "Номер";  // ✅ Добавлено соответствие свойству
             this.colRank.HeaderText = "№";
             this.colRank.Name = "colRank";
-            this.colRank.Width = 40;
+            this.colRank.Width = 50;
             this.colRank.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.colRank.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
 
-            // colCandidate
+            // Колонка "Друг"
             this.colCandidate.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colCandidate.DataPropertyName = "Друг";  // ✅ Добавлено соответствие свойству
+            this.colCandidate.FillWeight = 100;
             this.colCandidate.HeaderText = "Рекомендуемый друг";
             this.colCandidate.Name = "colCandidate";
-            this.colCandidate.DefaultCellStyle.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular);
+            this.colCandidate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            this.colCandidate.DefaultCellStyle.Padding = new Padding(10, 0, 10, 0);
+            this.colCandidate.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            this.colCandidate.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
 
-            // colMutual
+            // Колонка "Общих друзей"
             this.colMutual.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.colMutual.DataPropertyName = "ОбщихДрузей";  // ✅ Добавлено соответствие свойству
             this.colMutual.HeaderText = "Общих друзей";
             this.colMutual.Name = "colMutual";
-            this.colMutual.Width = 90;
+            this.colMutual.Width = 120;
             this.colMutual.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.colMutual.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
 
-            // colCoeff
+            // Колонка "Связность"
             this.colCoeff.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
-            this.colCoeff.HeaderText = "Связность";
+            this.colCoeff.DataPropertyName = "Связность";  // ✅ Добавлено соответствие свойству
+            this.colCoeff.HeaderText = "Коэффициент связности";
             this.colCoeff.Name = "colCoeff";
-            this.colCoeff.Width = 80;
+            this.colCoeff.Width = 150;
             this.colCoeff.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            this.colCoeff.DefaultCellStyle.Format = "P1";
+            this.colCoeff.DefaultCellStyle.Format = "P2";
+            this.colCoeff.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
 
-            // colFriends
+            // Колонка "Всего друзей"
             this.colFriends.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.colFriends.DataPropertyName = "ВсегоДрузей";  // ✅ Добавлено соответствие свойству
             this.colFriends.HeaderText = "Всего друзей";
             this.colFriends.Name = "colFriends";
-            this.colFriends.Width = 85;
+            this.colFriends.Width = 120;
             this.colFriends.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.colFriends.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
 
-            // groupBoxVisual - ИСПРАВЛЕНО: убраны FlatAppearance
-            this.groupBoxVisual.BackColor = panelColor;
-            this.groupBoxVisual.Controls.Add(this.pictureBoxNetwork);
-            this.groupBoxVisual.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
-            this.groupBoxVisual.ForeColor = primaryColor;
-            this.groupBoxVisual.Location = new System.Drawing.Point(398, 55);
-            this.groupBoxVisual.Name = "groupBoxVisual";
-            this.groupBoxVisual.Size = new System.Drawing.Size(520, 380);
-            this.groupBoxVisual.TabIndex = 2;
-            this.groupBoxVisual.TabStop = false;
-            this.groupBoxVisual.Text = "Визуализация социальной сети";
-
-            // pictureBoxNetwork
-            this.pictureBoxNetwork.BackColor = Color.White;
-            this.pictureBoxNetwork.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBoxNetwork.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBoxNetwork.Location = new System.Drawing.Point(3, 21);
-            this.pictureBoxNetwork.Name = "pictureBoxNetwork";
-            this.pictureBoxNetwork.Size = new System.Drawing.Size(514, 356);
-            this.pictureBoxNetwork.TabIndex = 0;
-            this.pictureBoxNetwork.TabStop = false;
-            this.pictureBoxNetwork.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxNetwork_Paint);
-
-            // groupBoxStats - ИСПРАВЛЕНО: убраны FlatAppearance
+            // groupBoxStats
             this.groupBoxStats.BackColor = panelColor;
             this.groupBoxStats.Controls.Add(this.lblStats);
             this.groupBoxStats.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
             this.groupBoxStats.ForeColor = primaryColor;
-            this.groupBoxStats.Location = new System.Drawing.Point(398, 441);
+            this.groupBoxStats.Location = new System.Drawing.Point(12, 517);
             this.groupBoxStats.Name = "groupBoxStats";
-            this.groupBoxStats.Size = new System.Drawing.Size(520, 160);
+            this.groupBoxStats.Size = new System.Drawing.Size(760, 100);
             this.groupBoxStats.TabIndex = 3;
             this.groupBoxStats.TabStop = false;
             this.groupBoxStats.Text = "Глобальная статистика сети";
 
-            // lblStats
             this.lblStats.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblStats.Font = new System.Drawing.Font("Consolas", 9.5F, System.Drawing.FontStyle.Regular);
+            this.lblStats.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular);
             this.lblStats.ForeColor = Color.FromArgb(64, 64, 64);
             this.lblStats.Location = new System.Drawing.Point(3, 21);
             this.lblStats.Name = "lblStats";
-            this.lblStats.Size = new System.Drawing.Size(514, 136);
+            this.lblStats.Size = new System.Drawing.Size(754, 76);
             this.lblStats.TabIndex = 0;
             this.lblStats.Text = "Нажмите кнопку \"Статистика сети\" для расчёта показателей социальной сети...";
-            this.lblStats.TextAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.lblStats.Padding = new Padding(10);
+            this.lblStats.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblStats.Padding = new Padding(15, 10, 10, 10);
 
             // Form1
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = backgroundColor;
-            this.ClientSize = new System.Drawing.Size(930, 613);
+            this.ClientSize = new System.Drawing.Size(784, 631);
             this.Controls.Add(this.groupBoxStats);
-            this.Controls.Add(this.groupBoxVisual);
             this.Controls.Add(this.groupBoxRecs);
             this.Controls.Add(this.groupBoxControls);
             this.Controls.Add(this.labelTitle);
@@ -272,19 +256,16 @@
             this.groupBoxControls.PerformLayout();
             this.groupBoxRecs.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRecs)).EndInit();
-            this.groupBoxVisual.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNetwork)).EndInit();
             this.groupBoxStats.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
         }
 
-        private System.Windows.Forms.GroupBox groupBoxControls, groupBoxRecs, groupBoxVisual, groupBoxStats;
+        private System.Windows.Forms.GroupBox groupBoxControls, groupBoxRecs, groupBoxStats;
         private System.Windows.Forms.ComboBox comboBoxUsers;
         private System.Windows.Forms.Button btnGenerateRecs, btnStats;
         private System.Windows.Forms.Label labelUser, labelTitle, lblStats;
         private System.Windows.Forms.DataGridView dataGridViewRecs;
         private System.Windows.Forms.DataGridViewTextBoxColumn colRank, colCandidate, colMutual, colCoeff, colFriends;
-        private System.Windows.Forms.PictureBox pictureBoxNetwork;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -18,22 +18,19 @@ namespace SocialNetwork.App
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // Инициализация сети при запуске
             _network.GenerateRandomNetwork(1000);
 
-            // Заполнение ComboBox с русскими именами
             comboBoxUsers.DisplayMember = "Value";
             comboBoxUsers.ValueMember = "Key";
             comboBoxUsers.DataSource = _network.GetAllUsers()
                 .Select(kvp => new { Key = kvp.Key, Value = kvp.Value.Name })
                 .ToList();
 
-            // Выбор первого пользователя по умолчанию
             if (comboBoxUsers.Items.Count > 0)
                 comboBoxUsers.SelectedIndex = 0;
         }
 
-        private void btnGenerateRecs_Click(object sender, EventArgs e)
+        private void BtnGenerateRecs_Click(object sender, EventArgs e)
         {
             if (comboBoxUsers.SelectedValue == null) return;
 
@@ -43,23 +40,21 @@ namespace SocialNetwork.App
             {
                 _currentRecommendations = _network.GetTopRecommendations(selectedUserId, 10);
 
-                // Привязка к DataGridView с нумерацией
                 var displayData = _currentRecommendations
                     .Select((rec, index) => new
                     {
-                        Rank = index + 1,
-                        Candidate = rec.CandidateName,
-                        MutualFriends = rec.MutualFriendsCount,
-                        Coefficient = rec.ConnectivityCoefficient,
-                        TotalFriends = rec.FriendsCount
+                        Номер = (index + 1).ToString(),
+                        Друг = rec.CandidateName,
+                        ОбщихДрузей = rec.MutualFriendsCount.ToString(),
+                        Связность = rec.ConnectivityCoefficient,
+                        ВсегоДрузей = rec.FriendsCount.ToString()
                     })
                     .ToList();
 
                 dataGridViewRecs.DataSource = null;
                 dataGridViewRecs.DataSource = displayData;
 
-                // Перерисовка визуализации
-                pictureBoxNetwork.Invalidate();
+                groupBoxRecs.Text = $"Топ-10 рекомендаций для: {_network.GetUser(selectedUserId)?.Name}";
             }
             catch (Exception ex)
             {
@@ -68,7 +63,7 @@ namespace SocialNetwork.App
             }
         }
 
-        private void btnStats_Click(object sender, EventArgs e)
+        private void BtnStats_Click(object sender, EventArgs e)
         {
             Cursor = Cursors.WaitCursor;
             var stats = _network.CalculateGlobalStatistics(10);
@@ -79,95 +74,6 @@ namespace SocialNetwork.App
                             $"✅ Пользователей с рекомендациями: {stats.UsersWithRecommendations}\n\n" +
                             $"📈 Процент пользователей с рекомендациями: {stats.PercentageWithRecommendations:F2}%\n\n" +
                             $"📊 Среднее количество рекомендаций на пользователя: {stats.AverageRecommendationsPerUser:F2}";
-        }
-
-        private void pictureBoxNetwork_Paint(object sender, PaintEventArgs e)
-        {
-            if (_currentRecommendations == null || _currentRecommendations.Count == 0 || comboBoxUsers.SelectedValue == null)
-            {
-                e.Graphics.DrawString("Выберите пользователя и нажмите «Найти друзей»",
-                    new Font("Segoe UI", 12), Brushes.Gray, 120, 160);
-                return;
-            }
-
-            int userId = (int)comboBoxUsers.SelectedValue;
-            var targetUser = _network.GetUser(userId);
-            if (targetUser == null) return;
-
-            var g = e.Graphics;
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-
-            int cx = pictureBoxNetwork.Width / 2;
-            int cy = pictureBoxNetwork.Height / 2;
-            int radiusFriends = 70;
-            int radiusRecs = 150;
-
-            // Рисуем связи с друзьями
-            var friends = targetUser.Friends.ToList();
-            for (int i = 0; i < friends.Count; i++)
-            {
-                double angle = 2 * Math.PI * i / Math.Min(friends.Count, 12);
-                int fx = cx + (int)(radiusFriends * Math.Cos(angle));
-                int fy = cy + (int)(radiusFriends * Math.Sin(angle));
-                g.DrawLine(new Pen(Color.FromArgb(100, 150, 200), 1.5f), cx, cy, fx, fy);
-            }
-
-            // Рисуем связи с рекомендованными кандидатами
-            foreach (var rec in _currentRecommendations)
-            {
-                double angle = 2 * Math.PI * rec.MutualFriendsCount / 10;
-                int rx = cx + (int)(radiusRecs * Math.Cos(angle));
-                int ry = cy + (int)(radiusRecs * Math.Sin(angle));
-
-                g.DrawLine(new Pen(Color.FromArgb(200, 150, 50), 2)
-                {
-                    DashStyle = System.Drawing.Drawing2D.DashStyle.Dash
-                }, cx, cy, rx, ry);
-            }
-
-            // Рисуем центрального пользователя
-            DrawNode(g, cx, cy, targetUser.Name.Split(' ')[0],
-                Color.FromArgb(0, 102, 204), Color.White, 25, true);
-
-            // Рисуем друзей (до 12)
-            for (int i = 0; i < Math.Min(friends.Count, 12); i++)
-            {
-                double angle = 2 * Math.PI * i / Math.Min(friends.Count, 12);
-                int fx = cx + (int)(radiusFriends * Math.Cos(angle));
-                int fy = cy + (int)(radiusFriends * Math.Sin(angle));
-                DrawNode(g, fx, fy, $"Друг {i + 1}",
-                    Color.FromArgb(100, 180, 220), Color.White, 10, false);
-            }
-
-            // Рисуем рекомендации
-            for (int i = 0; i < _currentRecommendations.Count; i++)
-            {
-                double angle = 2 * Math.PI * i / _currentRecommendations.Count;
-                int rx = cx + (int)(radiusRecs * Math.Cos(angle));
-                int ry = cy + (int)(radiusRecs * Math.Sin(angle));
-                var recName = _currentRecommendations[i].CandidateName.Split(' ');
-                string displayName = recName.Length > 0 ? recName[0] : "User";
-                DrawNode(g, rx, ry, displayName,
-                    Color.FromArgb(255, 180, 100), Color.White, 12, false);
-            }
-        }
-
-        private void DrawNode(Graphics g, int x, int y, string label,
-            Color fillColor, Color textColor, int size, bool isCenter)
-        {
-            // Рисуем круг
-            g.FillEllipse(new SolidBrush(fillColor), x - size, y - size, size * 2, size * 2);
-            g.DrawEllipse(new Pen(Color.FromArgb(100, 100, 100), 2), x - size, y - size, size * 2, size * 2);
-
-            // Рисуем текст
-            var font = new Font("Segoe UI", isCenter ? 10 : 7, FontStyle.Bold);
-            var textFormat = new StringFormat
-            {
-                Alignment = StringAlignment.Center,
-                LineAlignment = StringAlignment.Center
-            };
-            g.DrawString(label, font, new SolidBrush(textColor),
-                new RectangleF(x - size, y - size - 15, size * 2, size * 2), textFormat);
         }
     }
 }
